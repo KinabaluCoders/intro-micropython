8305aee01f2f0a6a20a359c5ac6c581415763a78
Edham Arief Dawillah / edthix <edthix@gmail.com>
2025-06-28 11:08:45 +0800

    Create 01_variables.py

---
+++ 01_variables.py
@@ -0,0 +1,70 @@
+# Variables : Variables are containers for storing data values.
+# Variable 1: Integer
+age = 30
+print("Variable Name: age")
+print("Value:", age)
+print("Type:", type(age))
+print("-" * 20)
+
+# Variable 2: Float
+price = 19.99
+print("Variable Name: price")
+print("Value:", price)
+print("Type:", type(price))
+print("-" * 20)
+
+# Variable 3: String
+name = "Alice"
+print("Variable Name: name")
+print("Value:", name)
+print("Type:", type(name))
+print("-" * 20)
+
+# Variable 4: Boolean
+is_student = True
+print("Variable Name: is_student")
+print("Value:", is_student)
+print("Type:", type(is_student))
+print("-" * 20)
+
+# Variable 5: List
+numbers = [1, 2, 3, 4, 5]
+print("Variable Name: numbers")
+print("Value:", numbers)
+print("Type:", type(numbers))
+print("-" * 20)
+
+# Variable 6: Tuple
+coordinates = (10.0, 20.0)
+print("Variable Name: coordinates")
+print("Value:", coordinates)
+print("Type:", type(coordinates))
+print("-" * 20)
+
+# Variable 7: Dictionary
+person = {"name": "Bob", "age": 25}
+print("Variable Name: person")
+print("Value:", person)
+print("Type:", type(person))
+print("-" * 20)
+
+# Variable 8: Set
+unique_colors = {"red", "blue", "green"}
+print("Variable Name: unique_colors")
+print("Value:", unique_colors)
+print("Type:", type(unique_colors))
+print("-" * 20)
+
+# Variable 9: NoneType
+empty_value = None
+print("Variable Name: empty_value")
+print("Value:", empty_value)
+print("Type:", type(empty_value))
+print("-" * 20)
+
+# Variable 10: Complex number
+complex_num = 3 + 4j
+print("Variable Name: complex_num")
+print("Value:", complex_num)
+print("Type:", type(complex_num))
+print("-" * 20)
